dfc72fd9a28c71ed7aab8c300c1bc1137076813c
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-02 15:46:23 +0100

    added so that first task that is not done is the power task

--- src/components/PowerTask/PowerTask.jsx
+++ src/components/PowerTask/PowerTask.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 const PowerTaskContainer = styled.div`
-    display: flex;
-    align-items: center;
-    `;
+  display: flex;
+  align-items: center;
+`;
 
 const PowerTaskItem = styled.h1``;
 
 const ClosePowerTaskButton = styled.button`
-    height: 50px;
-    margin-left: 50px;
-    `;
+  height: 50px;
+  margin-left: 50px;
+`;
 
 const PowerTask = (props) => {
-  const task = props.list[0].task;
+  const powerItem = props.list.find((item) => !item.isDone);
+  const task = powerItem.task;
   return (
     <PowerTaskContainer>
       <PowerTaskItem>{task}</PowerTaskItem>
