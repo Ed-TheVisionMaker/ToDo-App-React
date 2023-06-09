150e4f6db04c58292ca841c28e46b759fa4ee0c2
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 10:26:43 +0100

    CalendarDisplay from class to functional component. Cleaned up code

--- src/components/CalendarDisplay/CalendarDisplay.jsx
+++ src/components/CalendarDisplay/CalendarDisplay.jsx
@@ -1,42 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import Calendar from "react-calendar";
-import { formatDate } from "react-calendar/dist/cjs/shared/dateFormatter";
-
-// stylng react calendar
-// https://dev.to/fitzgeraldkd/react-calendar-with-custom-styles-30c9
 
 const CalendarContainer = styled.div`
   position: absolute;
   z-index: 1;
-
-  .react-calendar__month-view__weekdays__weekday {
-    // font-size: 10px;
-  }
 
   .react-calendar__month-view__days__day--neighboringMonth {
     opacity: 0.4;
   }
 `;
 
-export default class CalendarDisplay extends React.Component {
-  // formatShortWeekDay = (locale,_) => {
-    // console.log(date, "locale calendar display");
-  //   formatDate(locale, "dd");
-  // };
+const CalendarDisplay = (props) => {
+  const { handleClick, handleDateChange, id } = props;
+  return (
+    <CalendarContainer>
+      <Calendar
+        onClickDay={(date) => handleDateChange(id, date, handleClick)}
+      />
+    </CalendarContainer>
+  );
+};
 
-  render() {
-    const id = this.props.id;
-    const handleClick = this.props.handleClick;
-    return (
-      <CalendarContainer>
-        <Calendar
-          onClickDay={(date) =>
-            this.props.handleDateChange(id, date, handleClick)
-          }
-          // formatShortWeekday={this.formatShortWeekDay}
-        />
-      </CalendarContainer>
-    );
-  }
-}
+export default CalendarDisplay;
