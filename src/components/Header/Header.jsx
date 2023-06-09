67f121e49693f8ff681ed339c3509d20c779bf99
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 12:19:25 +0100

    amended style component name for Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
-const HeaderStyling = styled.h2`
+const Title = styled.h2`
     color: white;
     font-size: 36px;
 `
 
 const Header = () => {
     return (
-        <HeaderStyling>EssentiaList</HeaderStyling>
+        <Title>EssentiaList</Title>
     )
 }
 
