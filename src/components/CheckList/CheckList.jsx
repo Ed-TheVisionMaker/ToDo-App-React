671a9f5de0f8d21cbb54d795f225c4ee84609df1
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 10:45:46 +0100

    converted to functional component and cleaned up code

--- src/components/CheckList/CheckList.jsx
+++ src/components/CheckList/CheckList.jsx
@@ -32,42 +32,32 @@ const ChecklistItem = styled.li`
   margin-bottom: 20px;
 `;
 
-export default class Checklist extends React.Component {
-  // state = {
-  //   newItem: false,
-  // };
+const Checklist = (props) => {
+  const { handleChecklistNewItem, handleDisableAddCheckItem, item, newItem } =
+    props;
+  return (
+    <ChecklistContainer>
+      <AddChecklistItem
+        handleDisableAddCheckItem={handleDisableAddCheckItem}
+        handleChecklistNewItem={handleChecklistNewItem}
+        item={item}
+        newItem={newItem}
+      />
+      <ChecklistList>
+        {item.checklist.map((checklistItem) => {
+          return (
+            <ChecklistItem key={checklistItem.id}>
+              <ChecklistLine checklistItem={checklistItem} {...props} />
+            </ChecklistItem>
+          );
+        })}
+      </ChecklistList>
+      <CloseChecklist
+        handleChecklistClose={props.handleChecklistClose}
+        handleEnableAddCheckItem={props.handleEnableAddCheckItem}
+      />
+    </ChecklistContainer>
+  );
+};
 
-  // handleDisableAddCheckItem = () => {
-  //     this.setState({ newItem: true })
-  // }
-
-  // componentWillUnmount = () => {
-  //   console.log("checklist unmounted")
-  // }
-  
-  render() {
-    return (
-      <ChecklistContainer>
-        <AddChecklistItem
-          handleDisableAddCheckItem={this.props.handleDisableAddCheckItem}
-          handleChecklistNewItem={this.props.handleChecklistNewItem}
-          item={this.props.item}
-          newItem={this.props.newItem}
-        />
-        <ChecklistList>
-          {this.props.item.checklist.map((checklistItem) => {
-            return (
-              <ChecklistItem key={checklistItem.id}>
-                <ChecklistLine checklistItem={checklistItem} {...this.props} />
-              </ChecklistItem>
-            );
-          })}
-        </ChecklistList>
-        <CloseChecklist
-          handleChecklistClose={this.props.handleChecklistClose}
-          handleEnableAddCheckItem={this.props.handleEnableAddCheckItem}
-        />
-      </ChecklistContainer>
-    );
-  }
-}
+export default Checklist;
