83e6c943c79df54aa098b9124577a28b91db0ecf
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 11:37:41 +0100

    ChecklistLine changed to functional component

--- src/components/ChecklistLine/ChecklistLine.jsx
+++ src/components/ChecklistLine/ChecklistLine.jsx
@@ -4,29 +4,39 @@ import ChecklistCheckbox from "../ChecklistCheckbox/ChecklistCheckbox";
 import ChecklistInput from "../ChecklistInput/ChecklistInput";
 import ChecklistRemoveItem from "../ChecklistRemoveItem/ChecklistRemoveItem";
 
-export default class ChecklistLine extends React.Component {
-  state = {
-    inputValue: "",
-    isOpen: false,
-  };
+function ChecklistLine(props) {
+  const { checklistItem,
+          handleAmendCheckTask,
+          handleChecklistIsDone,
+          handleChecklistSubmit,
+          handleEnableAddCheckItem,
+          handleRemoveCheckItem,
+          indexOfItem,
+          item } = props;
 
-  render() {
-    const {checklistItem, handleEnableAddCheckItem, indexOfItem, item} = this.props;
-    const id = this.props.checklistItem.id;
-    return (
-      <>
-        <ChecklistCheckbox checklistItem={checklistItem} handleChecklistIsDone={this.props.handleChecklistIsDone} />
-        <ChecklistInput
-          checklistItem={checklistItem}
-          handleAmendCheckTask={this.props.handleAmendCheckTask}
-          item={item}
-          handleChecklistSubmit={this.props.handleChecklistSubmit}
-          handleEnableAddCheckItem={handleEnableAddCheckItem}
+  const id = checklistItem.id;
 
-        />
-        {/* <ChecklistRemoveItem id={id} handleRemoveCheckItem={this.props.handleRemoveCheckItem} /> */}
-        <ChecklistRemoveItem id={id} handleEnableAddCheckItem={handleEnableAddCheckItem} handleRemoveCheckItem={this.props.handleRemoveCheckItem} indexOfItem={indexOfItem} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <ChecklistCheckbox
+        checklistItem={checklistItem}
+        handleChecklistIsDone={handleChecklistIsDone}
+      />
+      <ChecklistInput
+        checklistItem={checklistItem}
+        handleAmendCheckTask={handleAmendCheckTask}
+        item={item}
+        handleChecklistSubmit={handleChecklistSubmit}
+        handleEnableAddCheckItem={handleEnableAddCheckItem}
+      />
+      <ChecklistRemoveItem
+        id={id}
+        handleEnableAddCheckItem={handleEnableAddCheckItem}
+        handleRemoveCheckItem={handleRemoveCheckItem}
+        indexOfItem={indexOfItem}
+      />
+    </>
+  );
+};
+
+export default ChecklistLine;
