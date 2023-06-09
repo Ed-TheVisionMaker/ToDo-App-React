a2262926eecb3958f00359423565afb94582cc6a
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 12:00:01 +0100

    Dropdown changed to functional component. Code cleaned up

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,38 +1,34 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
 const DdButton = styled.button`
   font-size: 16px;
 
   padding: 5px 10px;
-`
-
+`;
 
 const DdListContainer = styled.div`
   display: flex;
   flex-direction: column;
 
   position: absolute;
-  
-  // width: 100%
 
   padding-left: 15px;
   margin-top: 3px;
-`
+`;
 
-const DdListStyled = styled.ul`
+const DdList = styled.ul`
   display: flex;
   flex-direction: column;
 
   list-style-type: none;
 
   height: 80px;
-  // width: 100%;
   overflow: scroll;
   scrollbar-width: thin;
 
   border-radius: 5px;
-
 
   color: black;
   background-color: white;
@@ -42,7 +38,6 @@ const DdListValue = styled.li`
   display: flex;
   justify-content: center;
 
-  // width: 100%;
   padding: 0 5px;
 
   color: #242424;
@@ -56,40 +51,41 @@ const DdListValue = styled.li`
   }
 `;
 
+function Dropdown(props) {
+  const [listOpen, setListOpen] = useState(false);
 
-export default class Dropdown extends React.Component {
-  state = {
-    listOpen: false,
-  }
-
-  makeNumbersArray = (n) => {
+  const makeNumbersArray = (n) => {
     return Array(n)
-    .fill()
-    .map((_, i) => i + 1)
-  }
+      .fill()
+      .map((_, i) => i + 1);
+  };
 
-  handleOpen = (id, n, category) => {
-    this.setState((prevState) => ({ listOpen: !prevState.listOpen, }))
-    if(id) {
-      this.props.handleTaskValues(id, n, category)
+  const handleOpen = (id, n, category) => {
+    setListOpen(!listOpen);
+
+    if (id) {
+      props.handleTaskValues(id, n, category);
     }
-  }
+  };
 
+  const { id, item, category } = props;
 
-
-  render() {
-    const {id, item, category} = this.props
-    const listOpen = this.state.listOpen
-    return (
-      <div>
-        <DdButton onClick={() => this.handleOpen() }>{item[category]}</DdButton>
-        <DdListContainer>
-        {listOpen && ( <DdListStyled>
-        {this.makeNumbersArray(10).map((n) => <DdListValue key={n} onClick={() => this.handleOpen(id, n, category)} >{n}</DdListValue>)}
-      </DdListStyled>)}
-        </DdListContainer>
-
-      </div>
-    )
-  }
+  return (
+    <div>
+      <DdButton onClick={() => handleOpen()}>{item[category]}</DdButton>
+      <DdListContainer>
+        {listOpen && (
+          <DdList>
+            {makeNumbersArray(10).map((n) => (
+              <DdListValue key={n} onClick={() => handleOpen(id, n, category)}>
+                {n}
+              </DdListValue>
+            ))}
+          </DdList>
+        )}
+      </DdListContainer>
+    </div>
+  );
 }
+
+export default Dropdown;
