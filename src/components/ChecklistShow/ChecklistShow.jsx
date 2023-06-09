2058a6a299d0bcb2a4ba0e5c8d614165db2bd605
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 11:44:59 +0100

    ChecklistShow already a functional component. Cleaned up code

--- src/components/ChecklistShow/ChecklistShow.jsx
+++ src/components/ChecklistShow/ChecklistShow.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-const ChecklistShow = (props) =>  {
-    const item = props.item;
-    const checklistShow = props.item.checklist.length ? "Checklist" : "Add a checklist";
-    return <button onClick={() => props.handleChecklistClick(item)}>{checklistShow}</button>
-}
+const ChecklistShow = (props) => {
+  const { handleChecklistClick, item } = props;
+  const checklistShow = item.checklist.length ? "Checklist" : "Add a checklist";
+  return (
+    <button onClick={() => handleChecklistClick(item)}>{checklistShow}</button>
+  );
+};
 
 export default ChecklistShow;
