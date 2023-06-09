0c147db4eab46f669b89cb6c2b8b79f79d137bf6
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 10:33:00 +0100

    cleaned up code. Already a functinal component

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -1,39 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 
+const CheckboxButton = styled.button`
+  position: relative;
 
-const StyledCheckbox = styled.button`
-position: relative;
+  box-sizing: border-box;
+  width: 25px;
+  height: 25px;
 
-box-sizing: border-box;
-width: 25px;
-height: 25px;
-
-margin-left: 35px;
-margin-right: 70px;
+  margin-left: 35px;
+  margin-right: 70px;
 `;
 
-const StyledCheckboxSVG = styled.svg`
-position: absolute;
-top: -2px;
-left: -2px;
+const CheckboxSVG = styled.svg`
+  position: absolute;
+  top: -2px;
+  left: -2px;
 
-box-sizing: border-box;
-width: 25px;
-height: 25px;
+  box-sizing: border-box;
+  width: 25px;
+  height: 25px;
 
-color: black;
-background-color: white;
-border-radius: 5px;
+  color: black;
+  background-color: white;
+  border-radius: 5px;
 `;
 
 const Checkbox = (props) => {
-  const isDone = props.isDone;
-  
+  const { handleIsDone, id, isDone } = props;
   return (
-    <StyledCheckbox onClick={() => props.handleIsDone(props.id)}>
+    <CheckboxButton onClick={() => handleIsDone(id)}>
       {isDone && (
-        <StyledCheckboxSVG
+        <CheckboxSVG
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -45,9 +43,9 @@ const Checkbox = (props) => {
             strokeLinejoin="round"
             d="M4.5 12.75l6 6 9-13.5"
           />
-        </StyledCheckboxSVG>
+        </CheckboxSVG>
       )}
-    </StyledCheckbox>
+    </CheckboxButton>
   );
 };
 
