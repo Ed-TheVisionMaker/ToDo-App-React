507ec5e210632366e5234aa9cf3bb51c241cab1d
Ed Houlcroft <ed.lc.bwi@gmail.com>
2023-06-09 11:41:39 +0100

    CheckistRemoveItem already a functional component. Cleaned up code

--- src/components/ChecklistRemoveItem/ChecklistRemoveItem.jsx
+++ src/components/ChecklistRemoveItem/ChecklistRemoveItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledRemoveButton = styled.button`
+const RemoveButton = styled.button`
   position: relative;
 
   width: 30px;
@@ -11,11 +11,11 @@ const StyledRemoveButton = styled.button`
   padding: 5px;
   border-radius: 10px;
   border: 0;
-  
+
   background-color: transparent;
 `;
 
-const StyledRemoveSVG = styled.svg`
+const RemoveSVG = styled.svg`
   position: absolute;
   top: 0;
   left: 0;
@@ -27,22 +27,28 @@ const StyledRemoveSVG = styled.svg`
 `;
 
 const ChecklistRemoveItem = (props) => {
+  const { handleEnableAddCheckItem, handleRemoveCheckItem, id, indexOfItem } =
+    props;
   return (
-      <StyledRemoveButton onClick={() => props.handleRemoveCheckItem(props.id, props.indexOfItem, props.handleEnableAddCheckItem)}>
-        <StyledRemoveSVG
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </StyledRemoveSVG>
-      </StyledRemoveButton>
+    <RemoveButton
+      onClick={() =>
+        handleRemoveCheckItem(id, indexOfItem, handleEnableAddCheckItem)
+      }
+    >
+      <RemoveSVG
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </RemoveSVG>
+    </RemoveButton>
   );
 };
 
